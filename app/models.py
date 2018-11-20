ca4490ff51ec11a092c970dcb44a61db71b2bc9e
Robbe Van Herck <robbevanherck1@gmail.com>
2018-11-20 18:00:33 +0100

    Reduced size of url

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from .app import db
 class Service(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
-    url = db.Column(db.String(1024), unique=True, nullable=False)
+    url = db.Column(db.String(128), unique=True, nullable=False)
     # Broken since if now broken, else last date thing was broken
     broken_since = db.Column(db.DateTime)
     last_checked = db.Column(db.DateTime, default=datetime.fromtimestamp(0), nullable=False)
