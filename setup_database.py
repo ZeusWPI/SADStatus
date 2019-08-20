ebd0b1cffaa54ad261db406797cc948bc52b47ad
Robbe Van Herck <robbevanherck1@gmail.com>
2019-08-20 13:00:11 +0200

    Remove Slotmachien and add Cat

--- setup_database.py
+++ setup_database.py
@@ -10,11 +10,11 @@ services = {
     "Tab": "https://tab.zeus.gent",
     "Tap": "https://tap.zeus.gent",
     "Cammie": "https://zeus.ugent.be/cammie",
-    "Slotmachien": "https://kelder.zeus.ugent.be/slotmachien",
     "Wiki": "https://zeus.ugent.be/wiki",
     "Gamification": "https://zeus.ugent.be/game",
     "Mattermost": "https://mattermost.zeus.gent",
-    "Cammiechat": "https://kelder.zeus.ugent.be/messages"
+    "Cammiechat": "https://kelder.zeus.ugent.be/messages",
+    "Cat": "https://cat.zeus.gent"
 }
 
 for name, url in services.items():
